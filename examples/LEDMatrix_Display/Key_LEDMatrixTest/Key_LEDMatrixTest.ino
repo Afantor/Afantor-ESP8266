f4241a3ea32f7dbf3f6ae34ae9d267d41bb7391b
Afantor <13148946269@163.com>
2018-08-02 20:08:07 +0800

    add.

---
+++ examples/LEDMatrix_Display/Key_LEDMatrixTest/Key_LEDMatrixTest.ino
@@ -0,0 +1,37 @@
+/**
+ */
+#include "LEDMatrix.h"
+
+#define DATA_PIN 13
+#define CLK_PIN  0
+
+#define BUTTON0_PIN 16
+#define BUTTON0_PIN 0
+
+LEDMatrix ledMx(CLK_PIN,DATA_PIN);
+
+void setup()
+{
+  ledMx.setBrightness(6);
+  ledMx.setColorIndex(1);
+  pinMode(BUTTON0_PIN, INPUT);
+  pinMode(BUTTON1_PIN, INPUT);
+}
+
+char *s = "ABC";
+
+void loop()
+{
+	if(digitalRead(BUTTON0_PIN)==0)
+	{
+	    ledMx.showClock(12,03,1);
+  		// delay(2000);
+	}
+  	if(digitalRead(BUTTON1_PIN)==0)
+	{
+	    ledMx.drawStr(0,7,s);
+  		// delay(2000);
+	}
+  	ledMx.showNum(1.23);
+}
+
