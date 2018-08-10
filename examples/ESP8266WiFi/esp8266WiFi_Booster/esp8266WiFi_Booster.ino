01594aa3c7ab4cadc9f95e83faeccbd2d51df1b1
Afantor <13148946269@163.com>
2018-08-10 21:02:13 +0800

    add.

---
+++ examples/ESP8266WiFi/esp8266WiFi_Booster/esp8266WiFi_Booster.ino
@@ -0,0 +1,4 @@
+/********************
+教程1：https://blog.csdn.net/huahua974/article/details/80896760
+github：
+********************/
--- examples/LEDMatrix_Display/Key_LEDMatrixTest/Key_LEDMatrixTest.ino
+++ examples/LEDMatrix_Display/Key_LEDMatrixTest/Key_LEDMatrixTest.ino
@@ -6,7 +6,6 @@
 #define CLK_PIN  0
 
 #define BUTTON0_PIN 16
-#define BUTTON0_PIN 0
 
 LEDMatrix ledMx(CLK_PIN,DATA_PIN);
 
@@ -15,7 +14,6 @@ void setup()
   ledMx.setBrightness(6);
   ledMx.setColorIndex(1);
   pinMode(BUTTON0_PIN, INPUT);
-  pinMode(BUTTON1_PIN, INPUT);
 }
 
 char *s = "ABC";
@@ -27,11 +25,10 @@ void loop()
 	    ledMx.showClock(12,03,1);
   		// delay(2000);
 	}
-  	if(digitalRead(BUTTON1_PIN)==0)
-	{
-	    ledMx.drawStr(0,7,s);
-  		// delay(2000);
-	}
-  	ledMx.showNum(1.23);
+  else
+  {
+      ledMx.showNum(1.23);
+  }
+  
 }
 
